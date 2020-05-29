4d82414aab600ca2734cf4f1c5ba5dc30eb2eac1
Dawn Yue <1179348863@qq.com>
2020-05-29 15:02:14 +0800

    练习2

--- MeanShift/MeanShift.cpp
+++ MeanShift/MeanShift.cpp
@@ -12,10 +12,73 @@
 //练习2
 using namespace std;
 using namespace cv;
-
-int main()
+int templateMatching_demo()
 {
+	VideoCapture cap;
+	//cap(0)表示打开本机的第一个摄像头
+	cap.open(0);//cap(“D:\\1.avi”)本地视频
 
+	if (!cap.isOpened())
+	{
+		std::cout << "fail to open video...\n" << std::endl;
+		return -1;
+	}
+
+	Mat frame;
+	Mat tempMat;
+	Mat resultMat;
+	Mat refMat;
+	Mat dispMat;
+	int cnt = 0;
+	while (1) {
+
+		cap >> frame;
+		if (frame.empty())break;
+		//if (cnt == 0) { waitKey(10); }
+			if (cnt == 0) {
+			Rect2d r;
+			r = selectROI(frame, true);
+			tempMat = frame(r);
+			tempMat.copyTo(refMat);
+			destroyAllWindows();
+		}
+
+
+
+		int match_method = 0;
+		matchTemplate(frame, refMat, resultMat, match_method);
+
+		normalize(resultMat, resultMat, 0, 1, NORM_MINMAX, -1, Mat());
+
+		double minVal; double maxVal; Point minLoc; Point maxLoc;
+		Point matchLoc;
+
+		minMaxLoc(resultMat, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+
+		if (match_method == TM_SQDIFF || match_method == TM_SQDIFF_NORMED)
+		{
+			matchLoc = minLoc;
+		}
+		else
+		{
+			matchLoc = maxLoc;
+		}
+
+		frame.copyTo(dispMat);
+		rectangle(dispMat, matchLoc, Point(matchLoc.x + refMat.cols, matchLoc.y + refMat.rows), Scalar::all(0), 2, 8, 0);
+
+
+		cnt++;
+		imshow("template", refMat);
+		imshow("dispMat", dispMat);
+		waitKey(30);
+
+	}
+
+	return 0;
+}
+int img()
+{
 	cv::Mat img1 = cv::imread("E:\\13\\img.png");
 	Mat tempMat;
 	Mat resultMat;
@@ -60,4 +123,10 @@ int main()
 		waitKey(0);
 
 	return 0;
+}
+int main()
+{
+
+	templateMatching_demo();//视频匹配
+	//img();//图像匹配
 }
