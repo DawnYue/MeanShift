f9da27d385bcadb5971cb1ca03f3bbdf5a18f0df
Dawn Yue <1179348863@qq.com>
2020-05-29 14:13:07 +0800

    //练习1

--- MeanShift/MeanShift.cpp
+++ MeanShift/MeanShift.cpp
@@ -1,154 +1,109 @@
-﻿#include <iostream>
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
+﻿//练习1
+#include<opencv2/opencv.hpp>
+#include<iostream>
+#include<math.h>
 #include <vector>
-#include <core/core.hpp>
-#include <imgproc/imgproc.hpp>
-#include <highgui/highgui.hpp>
-#include <features2d/features2d.hpp>
-#include <opencv2/video/tracking.hpp>
-//练习1
-using namespace std;
+
 using namespace cv;
+using namespace std;
+
+void myharris(Mat & src, float * hist);
+
 int main()
 {
+	int bins = 1000000;
+	Mat src = imread("E:\\9\\hogTemplate.jpg");
+	Mat src1 = imread("E:\\9\\img1.jpg");
+	Mat src2 = imread("E:\\9\\img2.jpg");
 
-	cv::Mat mat_gray;
-	cv::Mat mat_binary;
-	cv::Mat mat_canny;
-	cv::Mat srcImage = imread("E:\\13\\img.png");
 
-	cvtColor(srcImage, mat_gray, COLOR_BGR2GRAY);//转化边缘检测后的图为灰度图
-	//形态学闭运算
-	cv::Mat elementRect;
-	elementRect = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3), cv::Point(-1, -1));
-	cv::morphologyEx(mat_gray, mat_gray, cv::MORPH_CLOSE, elementRect);
+	float * ref_hist = new float[bins];
+	memset(ref_hist, 0, sizeof(float)*bins);
 
-	// binary
-	cv::threshold(mat_gray, mat_binary, 125, 255.0, cv::THRESH_BINARY);
-	// detect edge
-	cv::Canny(mat_binary, mat_canny, 50, 125, 3);
+	float * ref_hist1 = new float[bins];
+	memset(ref_hist1, 0, sizeof(float)*bins);
 
-	// detect line
-	std::vector<cv::Vec4i> lines;
-	cv::HoughLinesP(mat_canny, lines, 1, CV_PI / 180, 35, 10, 10);
+	float * ref_hist2 = new float[bins];
+	memset(ref_hist2, 0, sizeof(float)*bins);
 
-	// draw line
-	for (size_t i = 0; i < lines.size(); i++)
-	{
-		cv::Vec4i linex = lines[i];
-		line(srcImage, cv::Point(linex[0], linex[1]), cv::Point(linex[2], linex[3]), cv::Scalar(0, 0, 255), 1, CV_AA);
+	myharris(src, ref_hist);
+	myharris(src1, ref_hist1);
+	myharris(src2, ref_hist2);
+
+
+	float sum1 = 0;
+	for (int i = 0; i < bins; i++) {
+		sum1 += (ref_hist[i] - ref_hist1[i])*(ref_hist[i] - ref_hist1[i]);
+	}
+	sum1 = sqrt(sum1);
+	cout << sum1 << endl;
+
+	float sum2 = 0;
+	for (int i = 0; i < bins; i++) {
+		sum2 += (ref_hist[i] - ref_hist2[i])*(ref_hist[i] - ref_hist2[i]);
+	}
+	sum2 = sqrt(sum2);
+	cout << sum2 << endl;
+
+	if (sum1 <= sum2) {
+		cout << "img1更与原图相似" << endl;
+	}
+	if (sum1 > sum2) {
+		cout << "img2更与原图相似" << endl;
 	}
 
-
-	cv::imshow("canny", mat_canny);
-	cv::imshow("mat", srcImage);
-	cv::waitKey(0);
-
+	delete[] ref_hist;
+	delete[] ref_hist1;
+	delete[] ref_hist2;
+	waitKey();
 	return 0;
-	/*/读取图片
-	cv::Mat img1 = cv::imread("E:\\13\\img.png");
-	cv::Mat img2 = cv::imread("E:\\13\\template.png");
-	cv::imshow("【被查找的图像】", img1);
-	cv::imshow("【模版图像】", img2);
-
-	//创建空画布绘制匹配结果
-	cv::Mat dstImg;
-	dstImg.create(img1.dims, img1.size, img1.type());
-	cv::imshow("createImg", dstImg);
-
-	//匹配
-	cv::matchTemplate(img1, img2, dstImg, 0);
-
-	//归一化图像矩阵，可省略
-	cv::normalize(dstImg, dstImg, 0, 1, 32);
-
-	//获取最大或最小匹配系数
-	//首先是从得到的 输出矩阵中得到 最大或最小值（平方差匹配方式是越小越好，所以在这种方式下，找到最小位置）
-	//找矩阵的最小位置的函数是 minMaxLoc函数
-	cv::Point minPoint;
-	cv::Point maxPoint;
-	double *minVal = 0;
-	double *maxVal = 0;
-	cv::minMaxLoc(dstImg, minVal, maxVal, &minPoint, &maxPoint);
-
-	//开始绘制
-	cv::rectangle(img1, minPoint, cv::Point(minPoint.x + img2.cols, minPoint.y + img2.rows), cv::Scalar(0, 255, 0), 2, 8);
-	cv::imshow("【匹配后的图像】", img1);
-	cv::rectangle(dstImg, minPoint, cv::Point(minPoint.x + img2.cols, minPoint.y + img2.rows), cv::Scalar(0, 0, 0), 3, 8);
-	cv::imshow("【匹配后的计算过程图像】", dstImg);
-	cv::waitKey(0);*/
-}
-/*
-Mat src, dst;
-int spr = 10, scr = 10, maxPryLevel = 3;
-//const Scalar& colorDiff=Scalar::all(1);
-
-
-void meanshift_seg(int, void *)
-{
-//调用meanshift图像金字塔进行分割
-pyrMeanShiftFiltering(src, dst, spr,scr, maxPryLevel);
-RNG rng = theRNG();
-Mat mask(dst.rows + 2, dst.cols + 2, CV_8UC1, Scalar::all(0));
-for (int i = 0; i<dst.rows; i++)    //opencv图像等矩阵也是基于0索引的
-for (int j = 0; j<dst.cols; j++)
-if (mask.at<uchar>(i + 1, j + 1) == 0)
-{
-Scalar newcolor(rng(256), rng(256), rng(256));
-floodFill(dst, mask, Point(j, i), newcolor, 0, Scalar::all(1), Scalar::all(1));//注意这里的 Point(j, i)的位置不要搞错，否则滑动条将不能正常化动 
-
-//原因是Point中的参数为x,y，对应到图片的矩阵里是列、行，而这段代码中遍历像素点时用的i,j，表示 //需要调换行、列，因此两者正好相反， 对于Mat的对象src，src.at(y.x)与src.at(Point(x,y))是等价的
-
-//        floodFill(dst,mask,Point(i,j),newcolor,0,colorDiff,colorDiff);
-}
-imshow("dst", dst);
 }
 
+void myharris(Mat & src, float * ref_hist) {
+	Mat gray;
+	cvtColor(src, gray, CV_BGR2GRAY);
 
+	int cellSize = 16;
+	float scale = 360 / 8;
+	int nX = gray.cols / cellSize;
+	int nY = gray.rows / cellSize;
 
+	Mat gx, gy;
+	Mat mag, angle;
+	cv::Sobel(gray, gx, CV_32F, 1, 0, 1);
+	cv::Sobel(gray, gy, CV_32F, 0, 1, 1);
+	cartToPolar(gx, gy, mag, angle, true);
 
+	int c = 0;
+	//遍历所有
+	for (int a = 0; ((a - 1)*nY) <= gray.rows; a++) {
+		for (int b = 0; ((b - 1)*nX) <= gray.cols; b++) {
 
+			//遍历其中一个cell
+			for (int j = (a*nY); j <= (a*nY + nY); j++) {
+				uchar* mag_row_ptr = mag.ptr <uchar>(j);// 第j行的头指针
+				uchar* angle_row_ptr = angle.ptr <uchar>(j);// 第j行的头指针
+				for (int i = (b*nX); i <= (b*nX + nX); i++) {
+					// 访问位于 x,y 处的像素
+					uchar* mag_ptr = &mag_row_ptr[i]; //  指向待访问的像素数据
+					uchar* angle_ptr = &angle_row_ptr[i]; //  指向待访问的像素数据
 
-int main(int argc, uchar* argv[])
-{
+					float data_mag = mag_ptr[0]; // data为I(x,y)第0个通道的值    	梯度
+					float data_angle = angle_ptr[0]; // data为I(x,y)第0个通道的值   角度
 
+					for (int i = 0; i <= 8; i++)
+					{
+						if (data_angle <= (i*scale))
+						{
+							int d = i + c;
+							ref_hist[d] = ref_hist[d] + data_mag;//遍历像素
+							//cout << "histgram" << ref_hist[d] << endl;
+						}
+					}
+				}
+			}
 
-namedWindow("src", WINDOW_AUTOSIZE);
-namedWindow("dst", WINDOW_AUTOSIZE);
-
-
-src = imread("stuff.jpg");
-CV_Assert(!src.empty());
-
-
-spr = 24;
-scr = 30;
-maxPryLevel = 3;
-
-
-//虽然createTrackbar函数的参数onChange函数要求其2个参数形式为onChange(int,void*)
-//但是这里是系统响应函数，在使用createTrackbar函数时，其调用的函数可以不用写参数，甚至
-//括号都不用写，但是其调用函数的实现过程中还是需要满足(int,void*)2个参数类型
-createTrackbar("spatialRad", "dst", &spr, 80, meanshift_seg);
-createTrackbar("colorRad", "dst", &scr, 80, meanshift_seg);
-createTrackbar("maxPryLevel", "dst", &maxPryLevel, 5, meanshift_seg);
-
-
-//    meanshift_seg(0,0);
-
-
-imshow("src", src);
-//char c=(char)waitKey();
-//if(27==c)
-//return 0;
-imshow("dst", src);
-waitKey();//无限等待用户交互响应
-//    while(1);//这里不能用while(1)的原因是需要等待用户的交互，而while(1)没有该功能。虽然2者都有无限等待的作用。
-return 0;
+			c = c + 8;
+		}
+	}
 }
-
-
-
-*/
